b01c3fc16dd93291c67bce80939d81283fb127b7
bjacob-56 <bjacob56@DESKTOP-HRLC1PI.localdomain>
2020-11-22 14:12:31 +0100

    maj norme1

--- get_next_line.c
+++ get_next_line.c
@@ -5,120 +5,120 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bjacob <bjacob@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/17 10:22:30 by bjacob            #+#    #+#             */
-/*   Updated: 2020/11/17 10:22:30 by bjacob           ###   ########lyon.fr   */
+/*   Created: 2020/11/22 14:08:27 by bjacob            #+#    #+#             */
+/*   Updated: 2020/11/22 14:10:18 by bjacob           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int init_gnl(int fd, char **line)
+int		init_gnl(int fd, char **line)
 {
-    if (!line || fd < 0 || !BUFFER_SIZE)
-    {
-        *line = NULL;
-        return (-1);
-    }
-    if (!(*line = ft_calloc(BUFFER_SIZE + 1)))
-        return (-1);
-    return (1);
+	if (!line || fd < 0 || !BUFFER_SIZE)
+	{
+		*line = NULL;
+		return (-1);
+	}
+	if (!(*line = ft_calloc(BUFFER_SIZE + 1)))
+		return (-1);
+	return (1);
 }
 
 ///// ---------- /////
 
-int ft_buffer_to_line(char **line, char *buffer)
+int		ft_buffer_to_line(char **line, char *buffer)
 {
-	int		i;
-    int     end_line;
+	int	i;
+	int	end_line;
 
 	i = 0;
-    end_line = 2;
+	end_line = 2;
 	while (buffer[i] != '\n' && buffer[i])
 	{
 		(*line)[i] = buffer[i];
 		i++;
 	}
 	(*line)[i] = '\0';
-    if (buffer[i] == '\n')
-        end_line = 1;
-    ft_crop_buffer(buffer, i + 1);
-    return (end_line);
+	if (buffer[i] == '\n')
+		end_line = 1;
+	ft_crop_buffer(buffer, i + 1);
+	return (end_line);
 }
 
 ///// ---------- /////
 
-void    ft_crop_buffer(char *buffer, int i_b)
+void	ft_crop_buffer(char *buffer, int i_b)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (i_b < BUFFER_SIZE + 1)
-    {
-        while (buffer[i_b + i])
-        {
-            buffer[i] = buffer[i_b + i];
-            i++;
-        }
-    }
-    while (i < BUFFER_SIZE + 1)
-        buffer[i++] = '\0';
+	i = 0;
+	if (i_b < BUFFER_SIZE + 1)
+	{
+		while (buffer[i_b + i])
+		{
+			buffer[i] = buffer[i_b + i];
+			i++;
+		}
+	}
+	while (i < BUFFER_SIZE + 1)
+		buffer[i++] = '\0';
 }
 
 ///// ---------- /////
 
-int ft_add_line(char **line, char *buffer)
+int		ft_add_line(char **line, char *buffer)
 {
-    int end_line;
-    int i_l;
-    int i_b;
-    char    *str_temp;
+	int		end_line;
+	int		i_l;
+	int		i_b;
+	char	*str_temp;
 
-    i_l = -1;
-    i_b = 0;
-    end_line = 2;
-    str_temp = *line;
-    if (!(*line = ft_calloc(ft_strlen(*line) + BUFFER_SIZE + 1)))
-        return (-1);
-    while ((str_temp)[++i_l])
-        (*line)[i_l] = str_temp[i_l];
-    while (buffer[i_b] != '\n' && buffer[i_b])
-    {
-        (*line)[i_l + i_b] = buffer[i_b];
-        i_b++;
-    }
-    if (buffer[i_b] == '\n')
-        end_line = 1;
-    ft_crop_buffer(buffer, i_b + 1);
-    free(str_temp);
-    return (end_line);
+	i_l = -1;
+	i_b = 0;
+	end_line = 2;
+	str_temp = *line;
+	if (!(*line = ft_calloc(ft_strlen(*line) + BUFFER_SIZE + 1)))
+		return (-1);
+	while ((str_temp)[++i_l])
+		(*line)[i_l] = str_temp[i_l];
+	while (buffer[i_b] != '\n' && buffer[i_b])
+	{
+		(*line)[i_l + i_b] = buffer[i_b];
+		i_b++;
+	}
+	if (buffer[i_b] == '\n')
+		end_line = 1;
+	ft_crop_buffer(buffer, i_b + 1);
+	free(str_temp);
+	return (end_line);
 }
 
 ///// ---------- /////
 
-int get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
-    static char buffer[BUFFER_SIZE + 1];
-    int         size;
+	static char	buffer[BUFFER_SIZE + 1];
+	int			size;
 
-    if (init_gnl(fd, line) == -1)
-        return (-1);
-    if (buffer[0])
-        if (ft_buffer_to_line(line, buffer) == 1)
-            return (1);
-    size = 1;
-    ft_bzero(buffer, BUFFER_SIZE + 1);
-    while (size > 0)
-    {
-        size = read(fd, buffer, BUFFER_SIZE);
-        if (size == -1)
-        {
-            ft_freeline(line);
-            return(-1);
-        }
-        if (ft_add_line(line, buffer) == 1)
-            return (1);
-    }
-//    if (!**line) // a voir si on return 0 sur la derniere ligne a lire ou juste apres   
-        return (0);
-    return (1);
+	if (init_gnl(fd, line) == -1)
+		return (-1);
+	if (buffer[0])
+		if (ft_buffer_to_line(line, buffer) == 1)
+			return (1);
+	size = 1;
+	ft_bzero(buffer, BUFFER_SIZE + 1);
+	while (size > 0)
+	{
+		size = read(fd, buffer, BUFFER_SIZE);
+		if (size == -1)
+		{
+			ft_freeline(line);
+			return (-1);
+		}
+		if (ft_add_line(line, buffer) == 1)
+			return (1);
+	}
+//	if (!**line) // a voir si on return 0 sur la ere ligne a lire ou juste apres   
+	return (0);
+	return (1);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjacob <bjacob@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/20 18:10:03 by bjacob            #+#    #+#             */
+/*   Updated: 2020/11/22 14:11:19 by bjacob           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int	ft_strlen(const char *str)
+int		ft_strlen(const char *str)
 {
-	int nb;
+	int	nb;
 
 	nb = 0;
 	while (*(str + nb) != '\0')
@@ -12,31 +24,31 @@ int	ft_strlen(const char *str)
 	return (nb);
 }
 
-char    *ft_calloc(size_t nmemb)
+char	*ft_calloc(size_t nmemb)
 {
-    char    *res;
-    size_t  i;
+	char	*res;
+	size_t	i;
 
-    if (!(res = malloc(sizeof(char) * nmemb)))
-        return (NULL);
-    i = 0;
-    while (i < nmemb)
-        res[i++] = '\0';
-    return (res);
+	if (!(res = malloc(sizeof(char) * nmemb)))
+		return (NULL);
+	i = 0;
+	while (i < nmemb)
+		res[i++] = '\0';
+	return (res);
 }
 
-void ft_bzero(char *s, int n)
+void	ft_bzero(char *s, int n)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < n)
-        s[i++] = '\0';
+	i = 0;
+	while (i < n)
+		s[i++] = '\0';
 }
 
-int     ft_freeline(char **line)
+int		ft_freeline(char **line)
 {
-    free(*line);
-    *line = NULL;
-    return (0);
+	free(*line);
+	*line = NULL;
+	return (0);
 }
